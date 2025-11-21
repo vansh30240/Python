33b80aafd968048fa3ebf3e02fb0737988b2f789
vansh <vanshnibhoria@gmail.com>
2025-11-21 22:17:13 +0530

    Add files via upload

---
+++ Lab 3/books.json.py
@@ -0,0 +1,32 @@
+[
+    {
+        "title": "Python fundamental",
+        "author": "Python Sir",
+        "isbn": "101",
+        "status": "available"
+    },
+    {
+        "title": "Introduction to c++",
+        "author": "rakesh Sir",
+        "isbn": "102",
+        "status": "issued"
+    },
+    {
+        "title": "ai algorithm",
+        "author": "AI Sir",
+        "isbn": "103",
+        "status": "available"
+    },
+    {
+        "title": "Computeshational Mathematics",
+        "author": "DR Arun Yadav Sir",
+        "isbn": "104",
+        "status": "available"
+    },
+    {
+        "title": "Introduction to HTML",
+        "author": "shadav Sir",
+        "isbn": "105",
+        "status": "available"
+    }
+]
